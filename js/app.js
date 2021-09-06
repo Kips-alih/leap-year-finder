eb8cba8e9aee6c451bc95dcfdf128a37cc4cf80d
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-06 22:22:59 +0300

    Modify the js file

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 function leapYear(){
   let year=document.getElementById("year").value;
   if((year%4==0)&&(year%100!=0)||(year%400==0)){
-    console.log(year+ " is a leap year");
+    document.getElementById("result").innerHTML= year+ " is a leap year";
     alert(year+ " is a leap year.")
 }
-  else{console.log(year+ "is not a leap year");
+  else{    document.getElementById("result").innerHTML= year+ " is not a leap year";
+  alert(year+ " is not a leap year.")
+
+  
     }
 };
